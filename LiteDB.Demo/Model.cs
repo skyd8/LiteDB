878affbfcb126b082d3478de4168952b9e85828f
skyd8 <35096955+skyd8@users.noreply.github.com>
2018-01-19 10:10:24 +0800

    Change demo for issue #861
    Change demo for issue #861

---
+++ LiteDB.Demo/Model.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiteDB.Demo
+{
+    public interface IBase
+    {
+        Guid Id { get; set; }
+        string Name { get; set; }
+    }
+
+    #region for test1
+    public interface IProject : IBase
+    {
+        string PorjectType { get; set; }
+        List<ISystem> Systems { get; set; }
+    }
+
+    public interface ISystem : IBase
+    {
+        string SysDefine { get; set; }
+        string SysMode { get; set; }
+    }
+
+    public class ProjectA : IProject
+    {
+        public ProjectA()
+        {
+        }
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+
+        public string PorjectType { get; set; }
+        public List<ISystem> Systems { get; set; }
+    }
+
+
+    public class SystemA : ISystem
+    {
+        public SystemA()
+        {
+        }
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+
+        public string SysDefine { get; set; }
+        public string SysMode { get; set; }
+    }
+
+    public class SystemB : ISystem
+    {
+        public SystemB()
+        {
+        }
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+
+        public string SysDefine { get; set; }
+        public string SysMode { get; set; }
+
+        public List<object> ItemsList { get; set; }
+    }
+
+    #endregion
+
+    #region for test2
+    public interface IProject2 : IBase
+    {
+        string PorjectType { get; set; }
+        List<ISystem2> Systems { get; set; }
+    }
+
+
+    public interface ISystem2 : IBase
+    {
+        Guid SysGuid { get; set; }
+        string SysDefine { get; set; }
+        string SysMode { get; set; }
+    }
+
+    public class ProjectA2 : IProject2
+    {
+        public ProjectA2()
+        {
+        }
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+
+        public string PorjectType { get; set; }
+        public List<ISystem2> Systems { get; set; }
+    }
+
+
+    public class SystemA2 : ISystem2
+    {
+        public SystemA2()
+        {
+        }
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid SysGuid { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+
+        public string SysDefine { get; set; }
+        public string SysMode { get; set; }
+    }
+
+    public class SystemB2 : ISystem2
+    {
+        public SystemB2()
+        {
+        }
+
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public Guid SysGuid { get; set; } = Guid.NewGuid();
+        public string Name { get; set; }
+
+        public string SysDefine { get; set; }
+        public string SysMode { get; set; }
+
+        public List<object> ItemsList { get; set; }
+    }
+    #endregion
+
+}
--- LiteDB.Demo/Program.cs
+++ LiteDB.Demo/Program.cs
@@ -13,78 +13,76 @@ namespace LiteDB.Demo
     {
         static void Main(string[] args)
         {
-            var timer = new Stopwatch();
-            ITest test = new LiteDB_Paging();
-            //ITest test = new SQLite_Paging();
+            Console.WriteLine("test1...\n");
 
-            Console.WriteLine("Testing: {0}", test.GetType().Name);
-
-            test.Init();
-
-            Console.WriteLine("Populating 100.000 documents...");
-
-            timer.Start();
-            test.Populate(ReadDocuments());
-            timer.Stop();
-
-            Console.WriteLine("Done in {0}ms", timer.ElapsedMilliseconds);
-
-            timer.Restart();
-            var counter = test.Count();
-            timer.Stop();
-            
-            Console.WriteLine("Result query counter: {0} ({1}ms)", counter, timer.ElapsedMilliseconds);
-            
-            var input = "0";
-            
-            while (input != "")
+            string filename = Path.Combine(Path.GetTempPath(), "temp.db");
+            using (var db = new LiteDatabase(filename))
             {
-                var skip = Convert.ToInt32(input);
-                var limit = 10;
-            
-                timer.Restart();
-                var result = test.Fetch(skip, limit);
-                timer.Stop();
-            
-                foreach(var doc in result)
+                try
                 {
-                    Console.WriteLine(
-                        doc["_id"].AsString.PadRight(6) + " - " +
-                        doc["name"].AsString.PadRight(30) + "  -> " +
-                        doc["age"].AsInt32);
+                    BsonMapper.Global.Entity<ISystem>().Id(x => x.Id);
+                    BsonMapper.Global.Entity<ProjectA>().Id(x => x.Id).DbRef(x => x.Systems);
+
+                    SystemA sysA = new SystemA() { Name = "SystemA", SysDefine = "SystemA Define", SysMode = "A mode" };
+                    SystemB sysB = new SystemB() { Name = "SystemB", SysDefine = "system B define", SysMode = "B mode", ItemsList = new List<object>() { 123 } };
+
+                    ProjectA proj = new ProjectA() { Name = "Project1", PorjectType = "ProjectType", Systems = new List<ISystem>() { sysA, sysB } };
+                    var proCol = db.GetCollection<ProjectA>("pros");
+                    var sysCol = db.GetCollection<ISystem>("systems");
+
+                    proCol.Insert(proj);
+                    sysCol.InsertBulk(proj.Systems);
+
+                    var projects = proCol.Include(x => x.Systems).FindAll();
                 }
-            
-                Console.Write("\n({0}ms) => Enter skip index: ", timer.ElapsedMilliseconds);
-                input = Console.ReadLine();
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"test1 exception: {ex.ToString()}");
+                }
+                finally
+                {
+                    db.DropCollection("pros");
+                    db.DropCollection("systems");
+                }
             }
-            
+            Console.WriteLine("\n");
+            Console.WriteLine("test2...\n");
+            using (var db = new LiteDatabase(filename))
+            {
+                try
+                {
+                    BsonMapper.Global.Entity<ISystem2>().Id(x => x.SysGuid);
+                    BsonMapper.Global.Entity<SystemA2>().Id(x => x.SysGuid).Ignore(x => x.Id);
+                    BsonMapper.Global.Entity<SystemB2>().Id(x => x.SysGuid).Ignore(x => x.Id);
+                    BsonMapper.Global.Entity<ProjectA2>().Id(x => x.Id).DbRef(x => x.Systems);
+
+                    SystemA2 sysA2 = new SystemA2() { Name = "SystemA", SysDefine = "SystemA Define", SysMode = "A mode" };
+                    SystemB2 sysB2 = new SystemB2() { Name = "SystemB", SysDefine = "system B define", SysMode = "B mode", ItemsList = new List<object>() { 123 } };
+
+                    ProjectA2 proj2 = new ProjectA2() { Name = "Project1", PorjectType = "ProjectType", Systems = new List<ISystem2>() { sysA2, sysB2 } };
+                    var proCol = db.GetCollection<ProjectA2>("pros");
+                    var sysCol = db.GetCollection<ISystem2>("systems");
+
+                    proCol.Insert(proj2);
+                    sysCol.InsertBulk(proj2.Systems);
+
+                    var projects = proCol.Include(x => x.Systems).FindAll().ToList();
+                    var count = projects[0].Systems.Count;
+                    if(count !=2) 
+                    Console.WriteLine($"Error: The Systems count = {count}, but it should be 2");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"test2 exception: {ex.ToString()}");
+                }
+                finally
+                {
+                    db.DropCollection("pros");
+                    db.DropCollection("systems");
+                }
+            }
             Console.WriteLine("End");
             Console.ReadKey();
-        }
-
-        static IEnumerable<BsonDocument> ReadDocuments()
-        {
-            using (var s = File.OpenRead(@"datagen.txt"))
-            {
-                var r = new StreamReader(s);
-
-                while(!r.EndOfStream)
-                {
-                    var line = r.ReadLine();
-
-                    if (!string.IsNullOrEmpty(line))
-                    {
-                        var row = line.Split(',');
-
-                        yield return new BsonDocument
-                        {
-                            ["_id"] = Convert.ToInt32(row[0]),
-                            ["name"] = row[1],
-                            ["age"] = Convert.ToInt32(row[2])
-                        };
-                    }
-                }
-            }
         }
     }
 }
